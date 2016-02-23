0bb06951c0e79cba0751f74959b99d829432a26c
Bruno <bruno.eng.comp@gmail.com>
2016-02-23 00:11:47 -0300

    Register custom marshaller to domain class Car

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,4 +1,5 @@
 import com.vaidegrails.Car
+import grails.converters.JSON
 
 class BootStrap {
 
@@ -7,6 +8,10 @@ class BootStrap {
         new Car(maker: "Audi", model: "RS6 MTM Clubsport", price: 500000, speed: 350, power: 537).save(failOnError: true)
         new Car(maker: "Mercedes-Benz", model: "C 63 AMG Edo Competition T-Model ", price: 280000, speed: 343, power: 441).save(failOnError: true)
         new Car(maker: "BMW", model: "M3 Manhart Racing V8R Biturbo ", price: 300000, speed: 338, power: 513).save(failOnError: true)
+
+        JSON.registerObjectMarshaller(Car, { car ->
+            return [id: car.id, model: car.model, 'speed (in kilometers)': car.speed]
+        })
     }
     def destroy = {
     }
